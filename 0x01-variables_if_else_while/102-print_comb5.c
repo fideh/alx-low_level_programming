67bb9ae3680a4adfe53cc3c628f032b3ee0ba61f
Fidelis Njoki <kagiafidelis3@gmail.com>
2023-06-16 15:15:29 -0700

    fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,30 +8,27 @@
  */
 int main(void)
 {
-    int i, j;
+	int num1, num2;
 
-    for (i = 0; i < 100; i++)
-    {
-        for (j = 0; j < 100; j++)
-        {
-            if (i < j)
-            {
-                putchar((i / 10) + 48);
-                putchar((i % 10) + 48);
-                putchar(' ');
-                putchar((j / 10) + 48);
-                putchar((j % 10) + 48);
+	for (num1 = 0; num1 <= 98; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-                if (i != 98 || j != 99)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
+			if (num1 != 98 || num2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-    putchar('\n');
-    return (0);
+	putchar('\n');
+	return (0);
 }
 
